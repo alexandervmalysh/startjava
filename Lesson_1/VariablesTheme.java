ffd7b11e4a594ece89ecd38fba83971148fed2d1
Alexander Malysh <alexandervmalysh@ya.ru>
2025-04-24 20:21:33 +0500

    Задача 7 - оптимизирован порядок вызова переменных. Обновлен порядок задач в соответствии с условиями

--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -8,7 +8,15 @@ public class VariablesTheme {
         final LocalTime startTime = LocalTime.now();
         final long startNano = System.nanoTime();
 
-        System.out.println("1. Расчет стоимости товара");
+        System.out.println("1. Вывод ASCII-графики");
+        System.out.println("""
+                                 /\\
+               J    a  v     v  /  \\
+               J   a a  v   v  /_( )\\
+            J  J  aaaaa  V V  /      \\
+             JJ  a     a  V  /___/\\___\\""");
+
+        System.out.println("\n2. Расчет стоимости товара");
         System.out.println("Первый способ решения:");
         float penPrice = 105.5f;
         float bookPrice = 235.23f;
@@ -33,15 +41,55 @@ public class VariablesTheme {
         System.out.println("Сумма скидки: " + dismountAmountBd + " руб.");
         System.out.println("Стоимость товара со скидкой: " + finalPriceBd + " руб.");
 
-        System.out.println("\n2. Вывод ASCII-графики");
-        System.out.println("""
-                                 /\\
-               J    a  v     v  /  \\
-               J   a a  v   v  /_( )\\
-            J  J  aaaaa  V V  /      \\
-             JJ  a     a  V  /___/\\___\\""");
+        System.out.println("\n3. Перестановка значений ячеек в таблице");
+        int numberOne = 2;
+        int numberTwo = 5;
+        System.out.println("Исходные значения переменных: ");
+        System.out.println("A = " + numberOne + ", B = " + numberTwo);
 
-        System.out.println("\n3. Тестирование датчиков перед запуском ракеты");
+        System.out.println("\nМетод: третьей переменной");
+        int temp = numberOne;
+        numberOne = numberTwo;
+        numberTwo = temp;
+        System.out.println("Результат: A1 = " + numberOne + ", B1 = " + numberTwo);
+
+        System.out.println("\nМетод: арифметических операций");
+        numberOne += numberTwo;
+        numberTwo = numberOne - numberTwo;
+        numberOne -= numberTwo;
+        System.out.println("Результат: A1 = " + numberOne + ", B1 = " + numberTwo);
+
+        System.out.println("\nМетод: побитовой операции");
+        numberOne ^= numberTwo;
+        numberTwo ^= numberOne;
+        numberOne ^= numberTwo;
+        System.out.println("Результат: A1 = " + numberOne + ", B1 = " + numberTwo);
+
+        System.out.println("\n4. Декодирование сообщения");
+        int code1 = 1055;
+        int code2 = 1088;
+        int code3 = 1080;
+        int code4 = 1074;
+        int code5 = 1077;
+        int code6 = 1090;
+        System.out.printf("%4d %4d %4d %4d %4d %4d%n", code1, code2, code3, code4, code5, code6);
+        System.out.printf("%4c %4c %4c %4c %4c %4c%n", code1, code2, code3, code4, code5, code6);
+
+        System.out.println("\n5. Анализ кода товара");
+        int productCode = 123;
+        int category = productCode / 100;
+        int subcategory = (productCode / 10) % 10;
+        int packageType = productCode % 10;
+        int checksum = category + subcategory + packageType;
+        int verificationCode = category * subcategory * packageType;
+        System.out.println("Код товара: " + productCode);
+        System.out.println("  категория товара - " + category);
+        System.out.println("  подкатегория - " + subcategory);
+        System.out.println("  тип упаковки - " + packageType);
+        System.out.println("Контрольная сумма = " + checksum);
+        System.out.println("Проверочный код = " + verificationCode);
+
+        System.out.println("\n6. Тестирование датчиков перед запуском ракеты");
         byte temperature = Byte.MAX_VALUE;
 
         System.out.println("[Температура, °C]");
@@ -73,61 +121,13 @@ public class VariablesTheme {
         System.out.println("+1: " + ++launchTime);
         System.out.println("-1: " + --launchTime);
 
-        System.out.println("\n4. Перестановка значений ячеек в таблице");
-        int numberOne = 2;
-        int numberTwo = 5;
-        System.out.println("Исходные значения переменных: ");
-        System.out.println("A = " + numberOne + ", B = " + numberTwo);
-
-        System.out.println("\nМетод: третьей переменной");
-        int temp = numberOne;
-        numberOne = numberTwo;
-        numberTwo = temp;
-        System.out.println("Результат: A1 = " + numberOne + ", B1 = " + numberTwo);
-
-        System.out.println("\nМетод: арифметических операций");
-        numberOne += numberTwo;
-        numberTwo = numberOne - numberTwo;
-        numberOne -= numberTwo;
-        System.out.println("Результат: A1 = " + numberOne + ", B1 = " + numberTwo);
-
-        System.out.println("\nМетод: побитовой операции");
-        numberOne ^= numberTwo;
-        numberTwo ^= numberOne;
-        numberOne ^= numberTwo;
-        System.out.println("Результат: A1 = " + numberOne + ", B1 = " + numberTwo);
-
-        System.out.println("\n5. Декодирование сообщения");
-        int code1 = 1055;
-        int code2 = 1088;
-        int code3 = 1080;
-        int code4 = 1074;
-        int code5 = 1077;
-        int code6 = 1090;
-        System.out.printf("%4d %4d %4d %4d %4d %4d%n", code1, code2, code3, code4, code5, code6);
-        System.out.printf("%4c %4c %4c %4c %4c %4c%n", code1, code2, code3, code4, code5, code6);
-
-        System.out.println("\n6. Анализ кода товара");
-        int productCode = 123;
-        int category = productCode / 100;
-        int subcategory = (productCode / 10) % 10;
-        int packageType = productCode % 10;
-        int checksum = category + subcategory + packageType;
-        int verificationCode = category * subcategory * packageType;
-        System.out.println("Код товара: " + productCode);
-        System.out.println("  категория товара - " + category);
-        System.out.println("  подкатегория - " + subcategory);
-        System.out.println("  тип упаковки - " + packageType);
-        System.out.println("Контрольная сумма = " + checksum);
-        System.out.println("Проверочный код = " + verificationCode);
-
         System.out.println("\n7. Вывод параметров JVM и ОС");
         Runtime rt = Runtime.getRuntime();
         double conversionMb = 1024.0 * 1024.0;
         int availableCpus = rt.availableProcessors();
         double totalMemory = rt.totalMemory() / conversionMb;
-        double usedMemory = (rt.totalMemory() - rt.freeMemory()) / conversionMb;
         double freeMemory = rt.freeMemory() / conversionMb;
+        double usedMemory = totalMemory - freeMemory;
         double maxMemory = rt.maxMemory() / conversionMb;
 
         System.out.println("Характеристики JVM:");
