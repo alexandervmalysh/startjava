fb871aef6f4d328331e1f5158e46df60328406f6
Alexander Malysh <alexandervmalysh@ya.ru>
2025-04-25 20:56:52 +0500

    Задача 1 решена

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -1,5 +1,13 @@
 public class CyclesTheme {
     public static void main(String[] args) {
+                System.out.println("1. Вывод ASCII-символов");
+        System.out.printf("%7s%12s%14s%n", "DECIMAL", "CHARACTER", "DESCRIPTION");
 
+        for (int i = 33; i < 123; i++) {
+            if ((i % 2 != 0 && i < 48) || 
+                    (i % 2 == 0 && i > 96 && i < 123)) {
+                System.out.printf("%4d%11c%12c%-20s%n", i, i, ' ', Character.getName(i));
+            }
+        }
     }
 }
