1a8fe8b593f7105f5eb4c90527c63bca5dee6e2c
Alexander Malysh <alexandervmalysh@ya.ru>
2025-05-02 13:01:49 +0500

    Задача Угадай число выполнена

---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int startInterval = 1;
+        int endInteraval = 100;
+        Random rnd = new Random();
+        int secretNumber = rnd.nextInt(startInterval, endInteraval);
+        int playerGuess = (startInterval + endInteraval) / 2;
+
+        while (true) {
+            if (playerGuess == secretNumber) {
+                System.out.println("Вы победили!");
+                break;
+            } else if (playerGuess > secretNumber) {
+                System.out.println(playerGuess + " больше того, что загадал компьютер");
+                endInteraval = playerGuess - 1;
+            } else {
+                System.out.println(playerGuess  + " меньше того, что загадал компьютер");
+                startInterval = playerGuess + 1;
+            }
+            playerGuess = (startInterval + endInteraval) / 2;
+        }
+    }
+}
