a47e8b8e6fd417781b745844c2f8e4e7e015db62
Alexander Malysh <alexandervmalysh@ya.ru>
2025-05-12 21:34:04 +0500

    Внесены исправления в соответствии с замечаниями

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,26 +1,75 @@
+import java.util.Scanner;
+
 public class GuessNumber {
     private Player player1;
     private Player player2;
     private int secretNumber;
+    private Scanner scanner;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
+        this.scanner = new Scanner(System.in);
     }
 
-    public void generateNumber() {
+    public void startGame() {
+        boolean continueGame = true;
+
+        while (continueGame) {
+            generateNumber();
+            playGame();
+
+            continueGame = askToContinue();
+        }
+    }
+
+    private void generateNumber() {
         secretNumber = (int) (Math.random() * 100 + 1);
     }
 
-    public boolean checkGuess(int guess) {
+    private void playGame() {
+        boolean isGuessed = false;
+        boolean isPlayer1Turn = true;
+
+        while (!isGuessed) {
+            Player currentPlayer = isPlayer1Turn ? player1 : player2;
+            System.out.print(currentPlayer.getName() + ", введите число для угадывания: ");
+            int playerGuess = scanner.nextInt();
+            isGuessed = checkGuess(playerGuess);
+
+            if (isGuessed) {
+                System.out.println("Поздравляем, " + currentPlayer.getName() +
+                        "! Вы угадали число и победили!");
+            } else {
+                giveHint(playerGuess);
+                isPlayer1Turn = !isPlayer1Turn;
+            }
+        }
+        scanner.nextLine();
+    }
+
+    private boolean checkGuess(int guess) {
         return guess == secretNumber;
     }
 
-    public void giveHint(int guess) {
+    private void giveHint(int guess) {
         if (guess < secretNumber) {
             System.out.println(guess + " меньше того, что загадал компьютер");
         } else if (guess > secretNumber) {
             System.out.println(guess + " больше того, что загадал компьютер");
+        }
+    }
+
+    private boolean askToContinue() {
+        while (true) {
+            System.out.println("Хотите продолжить игру? [yes/no]: ");
+            String answer = scanner.nextLine();
+
+            if (answer.equalsIgnoreCase("yes")) {
+                return true;
+            } else if (answer.equalsIgnoreCase("no")) {
+                return false;
+            }
         }
     }
 }
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -14,41 +14,6 @@ public class GuessNumberTest {
         Player player2 = new Player(player2Name);
 
         GuessNumber game = new GuessNumber(player1, player2);
-        boolean continueGame = true;
-
-        while (continueGame) {
-            boolean isGuessed = false;
-            boolean isPlayer1Turn = true;
-
-            game.generateNumber();
-
-            while (!isGuessed) {
-                Player currentPlayer = isPlayer1Turn ? player1 : player2;
-                System.out.print(currentPlayer.getName() + ", введите число для угадывания: ");
-                int playerGuess = scanner.nextInt();
-                isGuessed = game.checkGuess(playerGuess);
-
-                if (isGuessed) {
-                    System.out.println("Поздравляем, " + currentPlayer.getName() + 
-                            "! Вы угадали число и победили!");
-                } else {
-                    game.giveHint(playerGuess);
-                    isPlayer1Turn = !isPlayer1Turn;
-                }
-            }
-            scanner.nextLine();
-
-            while (true) {
-                System.out.println("Хотите продолжить игру? [yes/no]: ");
-                String answer = scanner.nextLine();
-
-                if (answer.equalsIgnoreCase("yes")) {
-                    break;
-                } else if (answer.equalsIgnoreCase("no")) {
-                    continueGame = false;
-                    break;
-                }
-            }
-        }
+        game.startGame();
     }
 }
