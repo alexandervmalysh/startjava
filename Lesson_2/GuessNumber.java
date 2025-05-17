e59f1ce15cadab96d250554db0bd2ae09ea68692
Alexander Malysh <alexandervmalysh@ya.ru>
2025-05-17 16:51:37 +0500

    Задание Угадай число исправлено в соответствии с замечаниями

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -4,52 +4,38 @@ public class GuessNumber {
     private Player player1;
     private Player player2;
     private int secretNumber;
-    private Scanner scanner;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
-        this.scanner = new Scanner(System.in);
     }
 
-    public void startGame() {
-        boolean continueGame = true;
-
-        while (continueGame) {
-            generateNumber();
-            playGame();
-
-            continueGame = askToContinue();
-        }
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        generateSecretNumber();
+        startGameplay(scanner);
     }
 
-    private void generateNumber() {
+    private void generateSecretNumber() {
         secretNumber = (int) (Math.random() * 100 + 1);
     }
 
-    private void playGame() {
-        boolean isGuessed = false;
-        boolean isPlayer1Turn = true;
+    private void startGameplay(Scanner scanner) {
+        Player currentPlayer = player1;
 
-        while (!isGuessed) {
-            Player currentPlayer = isPlayer1Turn ? player1 : player2;
+        while (true) {
             System.out.print(currentPlayer.getName() + ", введите число для угадывания: ");
             int playerGuess = scanner.nextInt();
-            isGuessed = checkGuess(playerGuess);
 
-            if (isGuessed) {
+            if (playerGuess == secretNumber) {
                 System.out.println("Поздравляем, " + currentPlayer.getName() +
                         "! Вы угадали число и победили!");
-            } else {
-                giveHint(playerGuess);
-                isPlayer1Turn = !isPlayer1Turn;
+                break;
             }
+            giveHint(playerGuess);
+            currentPlayer = (currentPlayer == player1) ? player2 : player1;
         }
         scanner.nextLine();
-    }
-
-    private boolean checkGuess(int guess) {
-        return guess == secretNumber;
     }
 
     private void giveHint(int guess) {
@@ -57,19 +43,6 @@ public class GuessNumber {
             System.out.println(guess + " меньше того, что загадал компьютер");
         } else if (guess > secretNumber) {
             System.out.println(guess + " больше того, что загадал компьютер");
-        }
-    }
-
-    private boolean askToContinue() {
-        while (true) {
-            System.out.println("Хотите продолжить игру? [yes/no]: ");
-            String answer = scanner.nextLine();
-
-            if (answer.equalsIgnoreCase("yes")) {
-                return true;
-            } else if (answer.equalsIgnoreCase("no")) {
-                return false;
-            }
         }
     }
 }
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -2,18 +2,25 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
 
         System.out.print("Введите имя первого игрока: ");
-        String player1Name = scanner.nextLine();
+        String player1Name = console.nextLine();
+        Player p1 = new Player(player1Name);
 
         System.out.print("Введите имя второго игрока: ");
-        String player2Name = scanner.nextLine();
+        String player2Name = console.nextLine();
+        Player p2 = new Player(player2Name);
 
-        Player player1 = new Player(player1Name);
-        Player player2 = new Player(player2Name);
+        GuessNumber game = new GuessNumber(p1, p2);
 
-        GuessNumber game = new GuessNumber(player1, player2);
-        game.startGame();
+        String answer;
+        do {
+            game.start();
+            do {
+                System.out.println("Хотите продолжить игру? [yes/no]: ");
+                answer = console.nextLine().toLowerCase();
+            } while (!"yes".equals(answer) && !"no".equals(answer));
+        } while (!"no".equals(answer));
     }
 }
