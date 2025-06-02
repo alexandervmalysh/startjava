e8466d99a5c4d066e993e1761b6c85c822061058
Alexander Malysh <alexandervmalysh@ya.ru>
2025-06-02 18:53:56 +0500

    Убраны статические переменные, кроме изменения цвета текста

--- src/com/github/alexandervmalysh/lesson_2_3_4/array/HackingAnimation.java
+++ src/com/github/alexandervmalysh/lesson_2_3_4/array/HackingAnimation.java
@@ -1,14 +1,9 @@
 package com.github.alexandervmalysh.lesson_2_3_4.array;
 
 public class HackingAnimation {
-    private static final char[] SPINNER_ELEMENTS = {'-', '\\', '|', '/'};
     private static final String ANSI_RED = "\033[0;31m";
     private static final String ANSI_GREEN = "\033[0;32m";
     private static final String ANSI_RESET = "\033[0m";
-    private static final int SPINNER_CYCLES = 3;
-    private static final int DELAY_MS = 250;
-    private static final int ACCESS_PROBABILITY = 70;
-    private static final String MSG = "Hacking: ";
 
     public static void main(String[] args) throws InterruptedException {
         simulateHacking();
@@ -16,16 +11,18 @@ public class HackingAnimation {
     }
 
     private static void simulateHacking() throws InterruptedException {
-        int len = SPINNER_ELEMENTS.length;
-        for (int i = 0; i < SPINNER_CYCLES * len; i++) {
-            System.out.print(MSG + SPINNER_ELEMENTS[i % len] + "\r");
-            Thread.sleep(DELAY_MS);
+        char[] spinnerElements = {'-', '\\', '|', '/'};
+        int len = spinnerElements.length;
+
+        for (int i = 0; i < 3 * len; i++) {
+            System.out.print("Hacking: " + spinnerElements[i % len] + "\r");
+            Thread.sleep(250);
         }
     }
 
     private static void checkAccess() {
-        boolean isAccessGranted = (int) (Math.random() * 100) > ACCESS_PROBABILITY;
-        System.out.printf(MSG + "%s%s", isAccessGranted ?
+        boolean isAccessGranted = (int) (Math.random() * 100) > 70;
+        System.out.printf("Hacking: %s%s", isAccessGranted ?
                 ANSI_GREEN + "Access Granted!" : ANSI_RED + "Access Denied!", ANSI_RESET);
     }
 }
