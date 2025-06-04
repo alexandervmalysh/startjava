0899854c04ba6655e9491c57cb2a91729b3f16fb
Alexander Malysh <alexandervmalysh@ya.ru>
2025-06-04 19:52:10 +0500

    Внесены изменения в соответствии с замечаниями

--- src/com/github/alexandervmalysh/lesson_2_3_4/array/HackingAnimation.java
+++ src/com/github/alexandervmalysh/lesson_2_3_4/array/HackingAnimation.java
@@ -1,28 +1,30 @@
 package com.github.alexandervmalysh.lesson_2_3_4.array;
 
 public class HackingAnimation {
-    private static final String ANSI_RED = "\033[0;31m";
-    private static final String ANSI_GREEN = "\033[0;32m";
-    private static final String ANSI_RESET = "\033[0m";
+    private static final String RED = "\033[0;31m";
+    private static final String GREEN = "\033[0;32m";
+    private static final String RESET = "\033[0m";
 
     public static void main(String[] args) throws InterruptedException {
-        simulateHacking();
-        checkAccess();
+        checkAccess(simulateHacking());
     }
 
-    private static void simulateHacking() throws InterruptedException {
-        char[] spinnerElements = {'-', '\\', '|', '/'};
-        int len = spinnerElements.length;
+    private static boolean simulateHacking() throws InterruptedException {
+        char[] spins = {'-', '\\', '|', '/'};
+        int len = spins.length;
 
+        System.out.print("Hacking: ");
         for (int i = 0; i < 3 * len; i++) {
-            System.out.print("Hacking: " + spinnerElements[i % len] + "\r");
+            System.out.print(spins[i % len]);
             Thread.sleep(250);
+            System.out.print("\b");
         }
+        return (int) (Math.random() * 100) > 70;
     }
 
-    private static void checkAccess() {
-        boolean isAccessGranted = (int) (Math.random() * 100) > 70;
-        System.out.printf("Hacking: %s%s", isAccessGranted ?
-                ANSI_GREEN + "Access Granted!" : ANSI_RED + "Access Denied!", ANSI_RESET);
+    private static void checkAccess(boolean isAccessGranted) {
+        System.out.printf("%s%s", isAccessGranted
+                ? GREEN + "Access Granted!"
+                : RED + "Access Denied!", RESET);
     }
 }
