c27571a05ad65b925e28c120e45796f0074ba398
Alexander Malysh <alexandervmalysh@ya.ru>
2025-06-17 11:44:55 +0500

    Урок 4, задание 5 - выполнено

---
+++ src/com/github/alexandervmalysh/lesson_2_3_4/array/CharTrianglePrinter.java
@@ -0,0 +1,48 @@
+package com.github.alexandervmalysh.lesson_2_3_4.array;
+
+public class CharTrianglePrinter {
+    public static void main(String[] args) {
+        StringBuilder charTriangle;
+
+        charTriangle = buildCharTriangle('0', '9', true);
+        printCharTriangle('0', '9', charTriangle);
+
+        charTriangle = buildCharTriangle('/', '!', false);
+        printCharTriangle('/', '!', charTriangle);
+
+        charTriangle = buildCharTriangle('A', 'J', false);
+        printCharTriangle('A', 'J', charTriangle);
+    }
+
+    private static StringBuilder buildCharTriangle(char leftBorder, char rightBorder, boolean direction) {
+        if (leftBorder > rightBorder) {
+            return null;
+        }
+
+        char[] chars = new char[rightBorder - leftBorder + 1];
+        int len = chars.length;
+
+        for (int i = 0; i < len; i++) {
+            chars[i] = direction ? leftBorder++ : rightBorder--;
+        }
+
+        StringBuilder charTriangle = new StringBuilder();
+
+        for (int i = 0; i < len; i++) {
+            charTriangle.repeat(" ", len - 1 - i);
+            charTriangle.append(chars[i]).repeat(chars[i], i * 2).append("\n");
+        }
+
+        return charTriangle;
+    }
+
+    private static void printCharTriangle(char leftBorder, char rightBorder, StringBuilder charTriangle) {
+        if (charTriangle == null) {
+            System.out.printf("Ошибка: левая граница %d > правой %d%n%n",
+                    (int) leftBorder, (int) rightBorder);
+            return;
+        }
+
+        System.out.println(charTriangle);
+    }
+}
