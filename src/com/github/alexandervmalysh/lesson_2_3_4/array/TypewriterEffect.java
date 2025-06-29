a32c0002e54bd83c06a0778838371422a420a282
Alexander Malysh <alexandervmalysh@ya.ru>
2025-06-29 09:04:22 +0500

    Замена многократного вызова replaceAll() на однократные вычисления длин слов

--- src/com/github/alexandervmalysh/lesson_2_3_4/array/TypewriterEffect.java
+++ src/com/github/alexandervmalysh/lesson_2_3_4/array/TypewriterEffect.java
@@ -4,20 +4,20 @@ public class TypewriterEffect {
     public static void main(String[] args) throws InterruptedException {
         String enteredText = "Java - это C++, из которого убрали все пистолеты, ножи и дубинки.\n" +
                 "- James Gosling";
-        type(toUpperCaseWordRange(enteredText));
+        type(toUpperCaseRange(enteredText));
 
         enteredText = "Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим его.\n" +
                 "- Robert Martin";
-        type(toUpperCaseWordRange(enteredText));
+        type(toUpperCaseRange(enteredText));
 
         enteredText = null;
-        type(toUpperCaseWordRange(enteredText));
+        type(toUpperCaseRange(enteredText));
 
         enteredText = "";
-        type(toUpperCaseWordRange(enteredText));
+        type(toUpperCaseRange(enteredText));
     }
 
-    private static String toUpperCaseWordRange(String enteredText) {
+    private static String toUpperCaseRange(String enteredText) {
         if (enteredText == null) {
             System.out.println("Ошибка: строка равна null\n");
             return null;
@@ -29,17 +29,22 @@ public class TypewriterEffect {
         }
 
         String[] words = enteredText.split(" ");
+        int[] cleanLengths = new int[words.length];
         int shortestWordIndex = 0;
         int longestWordIndex = 0;
 
         for (int i = 0; i < words.length; i++) {
             String cleanWord = words[i].replaceAll("\\p{P}", "");
-            if (cleanWord.isEmpty()) continue;
+            cleanLengths[i] = cleanWord.length();
+        }
 
-            if (cleanWord.length() < words[shortestWordIndex].replaceAll("\\p{P}", "").length()) {
+        for (int i = 0; i < cleanLengths.length; i++) {
+            if (cleanLengths[i] == 0) continue;
+
+            if (cleanLengths[i] < cleanLengths[shortestWordIndex]) {
                 shortestWordIndex = i;
             }
-            if (cleanWord.length() > words[longestWordIndex].replaceAll("\\p{P}", "").length()) {
+            if (cleanLengths[i] > cleanLengths[longestWordIndex]) {
                 longestWordIndex = i;
             }
         }
@@ -64,8 +69,8 @@ public class TypewriterEffect {
             return;
         }
 
-        for (String character : enteredText.split("")) {
-            System.out.print(character);
+        for (String ch : enteredText.split("")) {
+            System.out.print(ch);
             Thread.sleep(80);
         }
         System.out.println("\n");
