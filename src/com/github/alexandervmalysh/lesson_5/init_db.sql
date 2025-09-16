d4c3dd937619138da5fba31a14e471cf7628acc8
Alexander Malysh <alexandervmalysh@ya.ru>
2025-09-16 18:21:56 +0500

    Домашнее задание на тему "Базы данных" - внесены исправления в соответствии с замечаниями

--- src/com/github/alexandervmalysh/lesson_5/init_db.sql
+++ src/com/github/alexandervmalysh/lesson_5/init_db.sql
@@ -3,15 +3,15 @@
 DROP TABLE IF EXISTS jaegers;
 
 CREATE TABLE jaegers (
-    id          SERIAL          PRIMARY KEY,
-    model_name  VARCHAR(50)     NOT NULL,
-    mark        VARCHAR(10)     NOT NULL,
-    height      NUMERIC(6,3)    NOT NULL,
-    weight      NUMERIC(8,5)    NOT NULL,
-    status      VARCHAR(30)     NOT NULL,
-    origin      VARCHAR(30)     NOT NULL,
-    launch      DATE            NOT NULL,
-    kaiju_kill  INT             NOT NULL
+    id         SERIAL       PRIMARY KEY,
+    model_name VARCHAR(50)  NOT NULL,
+    mark       VARCHAR(10)  NOT NULL,
+    height     NUMERIC(6,3) NOT NULL,
+    weight     NUMERIC(8,5) NOT NULL,
+    status     VARCHAR(30)  NOT NULL,
+    origin     VARCHAR(30)  NOT NULL,
+    launch     DATE         NOT NULL,
+    kaiju_kill INT          NOT NULL
 );
 
 \i populate.sql
--- src/com/github/alexandervmalysh/lesson_5/queries.sql
+++ src/com/github/alexandervmalysh/lesson_5/queries.sql
@@ -1,57 +1,57 @@
--- Вывод всех роботов в алфавитном порядке по полю model_name
+\echo 'Вся таблица'
 SELECT *
   FROM jaegers
  ORDER BY model_name ASC;
 
--- Вывод всех роботов по полю model_name, которые не уничтожены
+\echo 'Не уничтоженные роботы'
 SELECT *
-FROM jaegers
-WHERE status = 'Active'
-ORDER BY model_name ASC;
+  FROM jaegers
+ WHERE status = 'Active'
+ ORDER BY model_name ASC;
 
--- Вывод всех роботов в алфавитном порядке по полю model_name серии Mark-1 и Mark-4
+\echo 'Роботы серии Mark-1 и Mark-4'
 SELECT *
-FROM jaegers
-WHERE mark IN ('1', '4')
-ORDER BY model_name ASC;
+  FROM jaegers
+ WHERE mark IN ('1', '4')
+ ORDER BY model_name ASC;
 
--- Вывод всех роботов, кроме серии Mark-1 и Mark-4 по убыванию поля mark
+\echo 'Все роботы, кроме Mark-1 и Mark-4, сортировка по убыванию по столбцу mark'
 SELECT *
-FROM jaegers
-WHERE mark NOT IN ('1', '4')
-ORDER BY mark DESC;
+  FROM jaegers
+ WHERE mark NOT IN ('1', '4')
+ ORDER BY mark DESC;
 
--- Вывод самых старых роботов в алфавитном порядке
+\echo 'Информация о самых старых роботах'
 SELECT *
   FROM jaegers
  WHERE launch = (SELECT MIN(launch) FROM jaegers)
  ORDER BY launch;
 
--- Вывод роботов в алфавитном порядке по полю model_name, которые уничтожили больше всех kaiju
+\echo 'Информация тех роботов, которые уничтожили больше всех kaiju'
 SELECT model_name, mark, launch, kaiju_kill
   FROM jaegers
  WHERE kaiju_kill = (SELECT MAX(kaiju_kill) FROM jaegers)
  ORDER BY model_name ASC;
 
--- Вывод среднего веса роботов с округлением до трех знаков после запятой
+\echo 'Средний вес роботов c округлением до трех знаков после запятой'
 SELECT ROUND(AVG(weight), 3) AS avg_weight
   FROM jaegers;
 
--- Увеличение количества убитых kaiju на 1 для всех неразрушенных роботов
+\echo 'Увеличение количества убитых kaiju на 1 у неразрушенных роботов'
 UPDATE jaegers
    SET kaiju_kill = kaiju_kill + 1
  WHERE status != 'Destroyed';
 
--- Вывод всех роботов в алфавитном порядке по полю model_name
+\echo 'Вся таблица'
 SELECT *
   FROM jaegers
  ORDER BY model_name ASC;
 
--- Удаление уничтоженных роботов
+\echo 'Удаление уничтоженных роботов'
 DELETE FROM jaegers
  WHERE status = 'Destroyed';
 
--- Вывод всех роботов после удаления в алфавитном порядке по полю model_name
+\echo 'Оставшиеся роботы'
 SELECT *
   FROM jaegers
  ORDER BY model_name ASC;
